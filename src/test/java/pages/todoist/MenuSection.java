e4229954f54a9d2f916048d541b4c42ddff958d2
RebecaNavarro <becanavarro2003@gmail.com>
2024-10-23 17:43:28 -0400

    Commit Numero 2

--- src/test/java/pages/todoist/MenuSection.java
+++ src/test/java/pages/todoist/MenuSection.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.By;
 public class MenuSection {
     public Button buttonWithName = new Button(By.xpath("//*[@id=':r0:']"));
 
-    public Button configurationButton = new Button(By.xpath("//*[@id=':rbq:']"));
+    public Button configurationButton = new Button(By.xpath("//div[contains(@id=':rd8:') and contains(@role='menuitem')]//span[@class='user_menu_label']"));
+//*[@id=":rd8:"]//*[@id=":rd8:"]/span
+    //[@class='user_menu_label']//span
+
     public Button logoutButton = new Button(By.xpath("//*[@id=':r48:']"));
 }
 
